d26779a39506326fa30b59b0f9a62c401d60dbe7
Tran Tan Dat <dattrantan01@gmail.com>
2022-12-13 18:21:08 +0700

    share modal show

--- src/components/post/ShareModal.js
+++ src/components/post/ShareModal.js
@@ -4,7 +4,7 @@ import Button from '../button/Button';
 
 const ShareModal = ({ handleClose, handleShare }) => {
   return ReactDOM.createPortal(
-    <div className="modal fixed inset-0 z-60 flex items-center justify-center p-5">
+    <div className="modal fixed inset-0 z-50 flex items-center justify-center p-5">
       <div className="absolute inset-0 bg-black bg-opacity-10 overlay"></div>
       <div className="relative z-10 rounded-lg w-full max-w-[300px] h-[150px]  bg-slate-100 px-5 py-5 flex flex-col">
         <h2 className="font-semibold text-center">
